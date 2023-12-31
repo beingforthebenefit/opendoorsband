b59079057b4e9e6e70ec4477150f3c69d02d3f8c
Gerald Todd <beingforthebenefit@gmail.com>
2023-12-30 23:42:31 -0800

    Add close menu button for mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger-menu');
     const navMenu = document.querySelector('.site-navigation');
+    const closeNav = document.querySelector('.close-nav');
     const mobileBreakpoint = 768; // Mobile breakpoint in pixels
 
     function toggleMobileNav() {
         if (window.innerWidth <= mobileBreakpoint) {
-            navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+            navMenu.classList.toggle('active');
         }
     }
 
     function handleResize() {
-        if (window.innerWidth > mobileBreakpoint) {
-            navMenu.style.display = ''; // Reset the display property
+        if (window.innerWidth > mobileBreakpoint && navMenu.classList.contains('active')) {
+            navMenu.classList.remove('active');
         }
     }
 
     hamburger.addEventListener('click', toggleMobileNav);
+    closeNav.addEventListener('click', toggleMobileNav);
     window.addEventListener('resize', handleResize);
 });
